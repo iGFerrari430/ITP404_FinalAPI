8fa54eb344b2c3ee59b38c70efad0103626d320a
Xiaowen Zhu <zhuxiaow@usc.edu>
2019-12-06 00:38:12 -0800

    ds

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const corsOptions = {
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 app.use(express.json());
