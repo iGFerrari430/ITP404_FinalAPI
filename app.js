7e1c9d5b82d3de9abdbbd3dfa676165cee7a6250
Xiaowen Zhu <zhuxiaow@usc.edu>
2019-12-05 22:46:44 -0800

    ve

--- app.js
+++ app.js
@@ -8,13 +8,8 @@ const corsOptions = {
   origin: 'http://hallowed-light.surge.sh'
 }
 
-//app.use(express.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-  next();
-});
+app.use(express.json());
 const {ENVIRONMENT,PORT} = process.env;
 const db = {
   posts: [
